9d71c39d8c68823ac5c2c7d74c89a4f55ded6730
Brian Ogilvie <brian@brianogilviedesigns.com>
2020-01-08 23:20:34 -0500

    divs 3D rotate on hover

---
+++ main.js
@@ -0,0 +1,55 @@
+const box = document.getElementById('box');
+const inputs = document.querySelectorAll('input');
+const hoverBoxes = document.querySelectorAll('.hover-box');
+
+const boxStyle = {
+  x: 0,
+  y: 0,
+  z: 0,
+  angle: 0
+};
+
+function handleSliderChange() {
+  const { name, value } = this;
+  boxStyle[name] = Number(value);
+  const { x, y, z, angle } = boxStyle;
+  box.style.transform = `rotate3d(${x}, ${y}, ${z}, ${angle}deg)`;
+}
+
+function transformHoverBox(e) {
+  // console.log(e.target);
+  const { pageX, pageY } = e;
+  const { x, y, width, height } = e.target.getBoundingClientRect();
+  // console.log({ rect });
+  // const { offsetLeft, offsetTop, offsetHeight, offsetWidth } = e.target;
+  // console.log({ pageY, offsetTop, offsetHeight });
+  const midX = x + width / 2;
+  const midY = y + height / 2;
+  const percentX = (pageX - midX) / (0.5 * width);
+  const percentY = (midY - pageY) / (0.5 * height);
+  const rotateX = `rotateX(${5 * percentY}deg)`;
+  const rotateY = `rotateY(${5 * percentX}deg)`;
+  e.target.style.transform = `${rotateX} ${rotateY}`;
+}
+
+function restoreHoverBox(e) {
+  e.target.classList.add('transition');
+  e.target.style.transform = 'none';
+  setTimeout(() => {
+    e.target.classList.remove('transition');
+  }, 200);
+}
+
+function enterHoverBox(e) {
+  e.target.classList.add('transition');
+  setTimeout(() => {
+    e.target.classList.remove('transition');
+  }, [200]);
+}
+
+inputs.forEach(input => input.addEventListener('input', handleSliderChange));
+hoverBoxes.forEach(hoverBox => {
+  hoverBox.addEventListener('mouseenter', enterHoverBox);
+  hoverBox.addEventListener('mousemove', transformHoverBox);
+  hoverBox.addEventListener('mouseleave', restoreHoverBox);
+});
